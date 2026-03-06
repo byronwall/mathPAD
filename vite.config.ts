ce8d3fa42e0595c07052b0120f03a0e8ef842b49
Byron Wall <byron@byroni.us>
2026-03-05 23:44:21 -0500

    Add storage UI and workflow fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [solid()],
   server: {
     port: 3000
